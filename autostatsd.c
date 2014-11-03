7bd16054137f0db6d1da8241ed4b009c09087a7f
Matt Lyndaker <mlyndaker@politico.com>
2014-11-03 16:01:46 -0500

    Add ability to record floating point values. Close stream on request shutdown.

--- autostatsd.c
+++ autostatsd.c
@@ -36,7 +36,7 @@ PHP_RINIT_FUNCTION(autostatsd)
 }
 
 static php_stream *stream = NULL;
-static char persistent_id[64] = {0};
+static char *stream_buffer = NULL;
 
 static char *host = "127.0.0.1";
 static int port = 8125;
@@ -51,31 +51,34 @@ void open_statsd_stream()
 	char *errstr;
 	int errcode;
 
-	stream = php_stream_xport_create(url, url_len, options, flags, persistent_id, NULL, NULL, &errstr, &errcode);
+	stream = php_stream_xport_create(url, url_len, options, flags, NULL, NULL, NULL, &errstr, &errcode);
 }
 
-void send_statsd_datagram(char *metric, int value, char *type)
+void close_statsd_stream()
+{
+	php_stream_close(stream);
+}
+
+void push_stat(char *metric, double val, char *type)
 {
 	char *data = NULL;
-	int data_len = spprintf(&data, 0, "%s:%d|%s", metric, value, type);
-
-    php_stream_write(stream, data, data_len);
-}
-
-float get_request_time()
-{
-
+	int data_len = spprintf(&data, 0, "%s:%g|%s", metric, val, type);
+	php_stream_write(stream, data, data_len);
 }
 
 PHP_RSHUTDOWN_FUNCTION(autostatsd)
 {
 	open_statsd_stream();
-	send_statsd_datagram("php.request.count", 1, "c");
-	send_statsd_datagram("php.request.memory.peak", zend_memory_peak_usage(0), "h");
-	send_statsd_datagram("php.request.memory.peak.real", zend_memory_peak_usage(1), "h");
-	send_statsd_datagram("php.request.memory.current", zend_memory_usage(0), "h");
-	send_statsd_datagram("php.request.memory.current.real", zend_memory_usage(1), "h");
 
+	push_stat("php.request.count", 1, "c");
+	push_stat("php.request.memory.peak", zend_memory_peak_usage(0), "h");
+	push_stat("php.request.memory.peak.real", zend_memory_peak_usage(1), "h");
+	push_stat("php.request.memory.current", zend_memory_usage(0), "h");
+	push_stat("php.request.memory.current.real", zend_memory_usage(1), "h");
+
+	//log_stat("php.request.time", Z_DVAL_P(zv_ptr), "h");
+
+	close_statsd_stream();
 
 	return SUCCESS;
 }
